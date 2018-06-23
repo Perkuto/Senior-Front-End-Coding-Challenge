b7bef565f88483994dff1c015e7973bf07928904
jwparadox <jingwenwang522@gmail.com>
2018-06-23 02:22:21 -0700

    dev add flicker image scroll app with angular-cli

--- README.md
+++ README.md
@@ -11,3 +11,25 @@ Fork this repository, complete challenge, submit pull request and provide produc
 
 ### Product Specifications
 * See wireframe.png
+
+### Angular Shareable image feed
+
+##Install
+1. Pull from branch
+2. Install lastest nodeJs if it's not there
+3. Run `npm install` in the root directory
+4. Run ` npm run build ` or `ng build` to build the project
+4. Run ` npm run start ` or `ng serve` to start the app
+5. Open browser and visit `http://localhost:4200/`
+
+## Example
+A working example can be found inside placeholder folder
+
+* Type in any keyword to start a search (based on flicker search api text search)
+* Address bar will be updated accordingly: e.g. `http://localhost:4200/#cat` for cat photos
+* Search result can be accessed through copy and then paste the sharable link directly
+* Scroll down to the bottom, more images will be loaded as scrolling until the end of result
+
+
+
+
---
+++ src/app/search/searchbar.ts
@@ -0,0 +1,79 @@
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { ImageService } from '../service/imagesearch.service';
+import {Location} from '@angular/common';
+import {Subscription} from 'rxjs';
+
+@Component({
+    selector: 'app-searchbar',
+    template: `<div class="ga-search"><input #searchInput 
+        type="search"
+        aria-label="search"
+        placeholder="Please enter your keyword!"
+        (keyup.enter)="updateSearch()">
+        <button [disabled]="loading"
+        (click)="updateSearch()">
+        Search</button>
+        <span class="loader" *ngIf="loading"></span></div>`,
+    styleUrls: ['./searchbar.component.css']
+})
+
+export class SearchBarComponent implements OnInit {
+    loading:boolean = false;
+    private subscription: Subscription;
+
+    @ViewChild('searchInput') searchInput: ElementRef;
+
+    constructor(
+        private imageService:ImageService,
+        private location: Location){
+            this.location = location;
+    }
+
+    ngOnInit(){
+        this.subscription = this.imageService.response
+                .subscribe((res)=>this._handleDataSuccess(res));
+        let deeplink = this._getSearch();
+        if (deeplink) {
+            this.query = deeplink;
+            this.updateSearch();
+        }    
+    }
+    
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
+    }
+
+    updateSearch(){
+        this.loading = true;
+        this.imageService.searchImage(this.query);
+    }
+
+    private _getSearch(){
+        let hash = window.location.hash;
+        let query = hash ? hash.split('#').pop() : null
+        return query ? decodeURIComponent(query) : null;
+    }
+
+    private _setSearch(query:string = ""){
+        const path = window.location.pathname;
+        this.location.replaceState(path + "#" + query);
+    }
+
+    private _handleDataSuccess(res){
+        if (!res) return;
+        if(res.stat === "ok"){
+            this.loading = false;
+            this._setSearch(this.query);
+        }else{
+            console.log("Request error:" + res);
+        }
+    }      
+
+    private get query(){
+        return this.searchInput.nativeElement.value;
+    }
+
+    private set query(val:string){
+        this.searchInput.nativeElement.value = val;
+    }
+}
