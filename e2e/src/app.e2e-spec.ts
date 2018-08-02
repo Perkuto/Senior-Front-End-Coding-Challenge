93a460b5b20c4960077994b0682c3b3df158cdf1
FaniloRandria <fanilo@gmail.com>
2018-08-01 21:27:29 -0400

    Add e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,29 @@ describe('flickr-pictures App', () => {
     page = new AppPage();
   });
 
-  it('should contain input type text', () => {
+  it('should display 100 photos on loading', () => {
     page.navigateTo();
-    expect(page.getPhotosList()).toBeDefined();
+    expect(page.getPhotosList().count()).toEqual(100);
   });
+
+  it('should display 2 photos when using a keyword in URL and should update the input text with the keyword', () => {
+    page.navigateTo('my%20keyword');
+    expect(page.getPhotosList().count()).toEqual(2);
+    expect(page.getInputText().getAttribute('value')).toEqual('my keyword');
+  });
+
+  it('should display 2 photos when typing a keyword in the input and should update the URL accordingly', () => {
+    page.navigateTo();
+    page.getInputText().sendKeys('another keyword');
+    expect(page.getPhotosList().count()).toEqual(2);
+    expect(page.getCurrentUrl()).toContain('another%20keyword');
+  });
+
+  it('should open a popup when clicking on twitter icon', () => {
+    page.navigateTo('bird');
+    const currentUrl = page.getCurrentUrl();
+    page.getTwitterButton().click();
+    expect(page.getWindowsCount()).toEqual(2);
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,35 @@
 import {browser, by, element} from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+
+  navigateTo(keyword: string = '') {
+    return browser.get('/#/' + keyword);
+  }
+
+  getCurrentUrl () {
+    return browser.getCurrentUrl();
+  }
+
+  getInputText() {
+    return element(by.css('input[type="text"]'));
+  }
+
+  getTextArea() {
+    return element(by.css('textarea'));
   }
 
   getPhotosList() {
-    return element(by.css('app-root app-photo-list')).getText();
+    return element.all(by.css('app-photo'));
   }
+
+  getTwitterButton() {
+    return element(by.css('share-button[button="twitter"]'));
+  }
+
+  getWindowsCount () {
+    return browser.getAllWindowHandles().then((handles) => {
+      return handles.length;
+    });
+  }
+
 }
