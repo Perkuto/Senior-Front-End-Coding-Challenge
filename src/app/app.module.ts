1f4dcc1ba98a587bd91c564b5af82617ab8f604a
bamb <bamb.bakang@gmail.com>
2019-10-14 12:29:09 -0400

    Added FormBuilder and PictureService to searchPictureComponent. Added searchPictureComponent into pictureWallComponent. Added onSubmit method that simulate Get request to flickr API.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,7 @@ import { PictureService } from './picture.service';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [PictureService],
--- src/app/search-pictures/search-pictures.component.ts
+++ src/app/search-pictures/search-pictures.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { PictureService } from '../picture.service';
 
 @Component({
   selector: 'app-search-pictures',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-pictures.component.css']
 })
 export class SearchPicturesComponent implements OnInit {
+  searchForm;
 
-  constructor() { }
+  constructor(
+    private pictureService: PictureService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.searchForm = this.formBuilder.group({
+      text: ''
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(searchText: string) {
+    // Send search text string to flickr API
+    console.warn('Search has been submitted', searchText);
+    this.searchForm.reset();
   }
 
 }
