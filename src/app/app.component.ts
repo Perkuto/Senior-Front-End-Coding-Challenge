c8791792366e030f22dbf94d421099276a29bd38
Christophe Durieux <christophe.durieux@icloud.com>
2019-12-01 17:44:13 -0500

    initial angular boilerplate with scss and routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cdx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Senior-Front-End-Coding-Challenge';
+}
